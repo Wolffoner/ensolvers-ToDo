142e09198899ea3a368dd2893b8bca52728a9675
Wolffoner <wolffoner@gmail.com>
2021-11-20 11:58:29 -0300

    folder annotations

--- taskToDo-Backend/src/main/java/com/example/taskToDo/models/Folder.java
+++ taskToDo-Backend/src/main/java/com/example/taskToDo/models/Folder.java
@@ -1,8 +1,38 @@
 package com.example.taskToDo.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "folder")
 public class Folder {
 
+  @Id
+  @GeneratedValue (strategy = GenerationType.IDENTITY)
   private Long id;
   private String title;
   private String description;
+
+  public Long getId() {
+    return this.id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return this.title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return this.description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
 }
