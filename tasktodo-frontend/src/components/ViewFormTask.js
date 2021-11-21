1559e0f1dc621700e9429b62cd10d1e4f9f90a31
Wolffoner <wolffoner@gmail.com>
2021-11-21 20:56:41 -0300

    view form

---
+++ tasktodo-frontend/src/components/ViewFormTask.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyledBackground } from './bg/StyledBackground';
+import {StyledBox} from './box/StyledBox';
+
+const ViewFormTask = () => {
+
+  return (
+    <>
+      <StyledBackground>
+        <StyledBox>
+        </StyledBox>
+      </StyledBackground> 
+    </>
+  );
+};
+
+export default ViewFormTask;
