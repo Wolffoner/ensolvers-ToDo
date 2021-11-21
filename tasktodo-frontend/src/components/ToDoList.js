87c307b9c1eabd946c1353bdd9fed74fb50fb582
Wolffoner <wolffoner@gmail.com>
2021-11-21 12:56:12 -0300

    minimal component todolist

---
+++ tasktodo-frontend/src/components/ToDoList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ToDoList = () => {
+  
+  return (
+    <>  
+      <table>
+        <tr>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Complete</th>
+          <th>Date Created</th>
+          <th>Date Finished</th>
+        </tr>
+      </table>
+    </>
+  );
+};
+
+export default ToDoList;
