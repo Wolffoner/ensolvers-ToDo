d30ecb932bdc20eabb776de1f0668e9790d1196c
Wolffoner <wolffoner@gmail.com>
2021-11-22 18:17:48 -0300

    80%

--- tasktodo-frontend/src/components/utils/folder/Folder.js
+++ tasktodo-frontend/src/components/utils/folder/Folder.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {Button} from '../button/Button';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Folder = ( {element = {
   id: '',
@@ -8,18 +9,26 @@ const Folder = ( {element = {
   description: '',
  }} ) => {
 
-  const [folder, setFolder] = useState({
+  const [folder] = useState({
     id: element?.id,
     title: element?.title,
     description: element?.description,
   });
+
+  const deleteFolder = async() =>{
+    try {
+      await axios.delete(`http://localhost:8080/folders/id=${folder.id}`);
+      window.location.reload();
+    } catch(err){
+    }
+  }
 
    return(
       <tr>
         <td>{folder?.title}</td>
         <td>{folder?.description}</td>
         <td><Link to='/list' state={folder.id}><Button title="🧾" color="#17845c"></Button></Link></td>
-        <td><Button title="🗑️" color="#ba1126"></Button></td>
+        <td><Button title="🗑️" color="#ba1126" onClick={deleteFolder}></Button></td>
       </tr> 
     )
   }
--- tasktodo-frontend/src/components/utils/task/Task.js
+++ tasktodo-frontend/src/components/utils/task/Task.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import axios from 'axios';
 import {Button} from '../button/Button';
-import { Link } from 'react-router-dom';
 
 const Task = ( {element = {
   id: '',
